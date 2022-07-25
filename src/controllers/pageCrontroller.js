2552fd057e0598b420e57c5a494d5fe77491fdd5
Matheus <giehlmatheus96@gmail.com>
2022-07-24 21:59:22 -0300

    update pageCrontroller.js

--- src/controllers/pageCrontroller.js
+++ src/controllers/pageCrontroller.js
@@ -1,19 +1,44 @@
 const express = require('express');
+const {createMenuObject} = require('../helpers/createMenuObject');
 
 const home = (req, res) => {
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu:createMenuObject('all'),
+        banner: {
+            title:'Todos os Animais',
+            background : 'allanimals.jpg'
+        }
+    })
 }
 
 const dogs = (req, res) => {
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu:createMenuObject('dog'),
+        banner: {
+            title:'Cachorros',
+            background : 'banner_dog.jpg'
+        }
+    })
 }
 
 const cats = (req, res) => {
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu:createMenuObject('cat'),
+        banner: {
+            title:'Todos os Animais',
+            background : 'banner_cat.jpg'
+        }
+    })
 }
 
 const fishes = (req, res) => {
-    res.render('pages/page')
+    res.render('pages/page',{
+        menu:createMenuObject('fish'),
+        banner: {
+            title:'Todos os Animais',
+            background : 'banner_fish.jpg'
+        }
+    })
 }
 
 module.exports.home = home;
